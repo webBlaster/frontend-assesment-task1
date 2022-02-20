79ad27319078d3aa028aa7e4625c74e9bb969d69
Yekinni Ajibola <yekinnijibola@gmail.com>
2022-02-20 10:37:49 +0100

    added remove todo functionality

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,10 +1,13 @@
-const TodoList = ({ lists }) => {
+const TodoList = ({ lists, removeItem }) => {
   return (
     <>
       {lists.length > 0 ? (
         lists.map((list) => (
           <div className="card todo" key={list.id}>
-            <a class="btn-floating btn-small waves-effect waves-light red right-align">
+            <a
+              class="btn-floating btn-small waves-effect waves-light red right-align"
+              onClick={() => removeItem(list.id)}
+            >
               <i class="material-icons">remove</i>
             </a>
             <h4>{list.title}</h4>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,20 +4,25 @@ import Header from "../components/header";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
+  const addItem = () => {};
+  const removeItem = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
   useEffect(async () => {
     //fetch todo
     let response = await fetch("https://jsonplaceholder.typicode.com/posts");
     if (response) {
       let result = await response.json();
-      console.log(result);
-      setTodos(result);
+      console.log(result.slice(0, 5));
+      setTodos(result.slice(0, 5));
     }
   }, []);
   return (
     <>
       <Header />
       <div className="container todo-container">
-        <TodoList lists={todos} />
+        <TodoList lists={todos} removeItem={removeItem} />
       </div>
     </>
   );
